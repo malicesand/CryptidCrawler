d609183bcb25ea67723125b982af4a079a9dd0db
maryalice <malicesand@gmail.com>
2024-11-14 16:48:56 -0600

    $tweet ID and CSS

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ $(document).ready(() => {
     ($body).prepend($tweetSection);
       // tweetSection CSS
     ($tweetSection).css({background: 'pink', maxWidth: '500px', border: '15px solid blue'})
-
-  function addNewTweets() { // 
-    const $tweets = streams.home.map(tweet => { // not sure if we need this but it's staying
-      const $tweet = $('<div></div>');
-      const text = `@${tweet.user}: ${tweet.message}`;
+    
+    function addNewTweets() { // 
+      const $tweets = streams.home.map(tweet => { // not sure if we need this but it's staying
+        const $tweet = $('<div id = "tweet"></div>');
+        const text = `@${tweet.user}: ${tweet.message}`;
+        $('#tweet').css({color: 'green'})
 
       $tweet.text(text);  // IMPORTANT for array or individual
       $tweetSection.prepend($tweet); // add individual tweet to beginning of tweetSection
