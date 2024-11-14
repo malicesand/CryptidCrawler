c88cb6bf3be42c6de6b47692e4f307e6793ac683
maryalice <malicesand@gmail.com>
2024-11-14 16:01:35 -0600

    create tweetSection, addNewTweets function, basic CSS

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 
 $(document).ready(() => {
+  // $('head').append($("<h1>").text("My Heading"))
+  // $('<h1>').css({color: "black", fontSize: "40px"})
+
+
   const $body = $('body');
   $body.html(''); // clears the body .html is sort of like a .replace
+  // const message = randomMessage
+  // const newTweet = generateRandomTweet
 
-  const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div></div>');
-    const text = `@${tweet.user}: ${tweet.message}`;
+  const $tweetSection = $('<section id = "tweet-section" class = "tweets">');
+  ($body).prepend($tweetSection);
+  ($tweetSection).css({background: 'pink', border: 'border: 1rem solid;'})
 
-    $tweet.text(text);
+  function addNewTweets() {
+    const $tweets = streams.home.map(tweet => {
+      const $tweet = $('<div></div>');
+      const text = `@${tweet.user}: ${tweet.message}`;
 
-    return $tweet;
-  });
-  $body.append($tweets);
+      $tweet.text(text);
 
-  //all code goes above this line!!
+       $tweetSection.prepend($tweet);
+    });
+
+  }
+
+  addNewTweets();
+
+($body).css({background: "green"})
+
+// ($tweets).css({text: "white"})
+
+
+// const heading1 = $("<h1>").text("My Heading");
+
 });
