70b722af80d8a5ed8b12c7e5ac8708679c14bd7f
maryalice <malicesand@gmail.com>
2024-11-15 17:44:34 -0600

    changed font tweetSection title

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ $(document).ready(() => {
       // place tweetSection at beginning of body
     ($body).prepend($tweetSection);
       // tweetSection CSS
-    ($tweetSection).css({background: 'pink', maxWidth: '500px', border: '15px solid blue'})
+    ($tweetSection).css({background: 'pink', maxWidth: '500px', horizontalAlign: 'center'})
     
     function addNewTweets() { // 
       const $tweets = streams.home.map(tweet => { // not sure if we need this but it's staying
@@ -39,9 +39,9 @@ const $heading = $("<h1 id= 'main-head'>CRYPTID CRAWLER</h1>");  // header for p
   ($body).prepend($heading) // header to top of page
   $('#main-head').css({fontFamily: 'melted monster', color: 'magenta', fontSize: '100px', textAlign: 'center'})
 
-const $tweetTitle = $("<h2 id = 'tweetTitle'>Twiddles</h2>"); // title for tweet box
+const $tweetTitle = $("<h2 id = 'tweetTitle'>muttering</h2>"); // title for tweet box
   ($tweetSection).prepend($tweetTitle);
-  ($tweetTitle).css({color: 'blue', fontSize: '14 px', padding: '14px'})
+  ($tweetTitle).css({fontFamily: 'melted monster', color: 'teal', fontSize: '14 px', padding: '14px', textAlign: 'center'})
 
 //all code above this line !!!
 });
